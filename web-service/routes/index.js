9198e8a9673ffa4610f3a4cfded6d31ba3844631
Guillem Ramirez Miranda <19.guillemramirez19@gmail.com>
2019-04-06 17:50:39 +0200

    pipo

--- web-service/routes/index.js
+++ web-service/routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/viewer', function(req, res, next) {
+  res.render('viewer', { title: 'Viewer' });
+}
 module.exports = router;
